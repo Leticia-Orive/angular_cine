ececa4474166cf1a796f9bbbd9ea2c367992320f
LETICIA ORIVE PEÑA <leticia.orive.contractor@bbva.com>
2023-06-14 10:13:01 +0200

    vistas de cine

---
+++ cine/src/app/cine/vistas/cine-create.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CineCreateComponent } from './cine-create.component';
+
+@NgModule({
+  declarations: [CineCreateComponent],
+  imports: [CommonModule, FormsModule],
+})
+export class CineCreateModule {}
